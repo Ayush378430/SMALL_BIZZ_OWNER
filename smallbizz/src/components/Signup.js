2cf4c30c142491ea58dadc6cbc1fac86a6ddd2b9
Ayush-Gurav <ayushgurav6@gmail.com>
2024-06-20 14:42:14 +0530

    goverment id done

--- smallbizz/src/components/Signup.js
+++ smallbizz/src/components/Signup.js
@@ -3,43 +3,40 @@ import styles from "./Signup.module.css";
 import { useState } from "react";
 import axios from "axios";
 
-
 const Signup = () => {
-  const [data,setData]=useState({
-    shopName:"",
-    email:"",
-    password:"",
-    ownerName:"",
-    mobileNo:"",
-    pinCode:"",
-    address:"",
-    category:""
+  const [data, setData] = useState({
+    shopName: "",
+    gov_id: "",
+    email: "",
+    password: "",
+    ownerName: "",
+    mobileNo: "",
+    pinCode: "",
+    address: "",
+    category: ""
   });
-  const [error,setError]=useState("");
-  const navigate=useNavigate();
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-
-  const handleChange = ({currentTarget:input}) => {
-    setData({...data,[input.name]:input.value});
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url="http://localhost:8000/api/shops";
-      const {data:res}=await axios.post(url,data);
+      const url = "http://localhost:8000/api/shops";
+      const { data: res } = await axios.post(url, data);
       navigate("/login");
       console.log(res.message);
-    }
-    catch (error) {
-      if(error.response && error.response.status >= 400 && error.response.status <= 500)
-      {
+    } catch (error) {
+      if (error.response && error.response.status >= 400 && error.response.status <= 500) {
         setError(error.response.data.message);
       }
     }
-  }
+  };
 
-  return(
+  return (
     <div className={styles.signup_container}>
       <div className={styles.signup_form_container}>
         <div className={styles.left}>
@@ -52,7 +49,7 @@ const Signup = () => {
         </div>
         <div className={styles.right}>
           <form className={styles.form_container} onSubmit={handleSubmit}>
-            <h1>Create Account</h1>
+            <h1>Create Shop</h1>
             <input
               type="text"
               placeholder="Shop Name"
@@ -80,61 +77,69 @@ const Signup = () => {
               required
               className={styles.input}
             />
-              
-            
             <input
-            type="text"
-            placeholder="Owner Name"
-            name="ownerName"
-            onChange={handleChange}
-            value={data.ownerName}
-            required
-            className={styles.input}
+              type="text"
+              placeholder="Owner Name"
+              name="ownerName"
+              onChange={handleChange}
+              value={data.ownerName}
+              required
+              className={styles.input}
             />
             <input
-            type="text"
-            placeholder="Mobile No"
-            name="mobileNo"
-            onChange={handleChange}
-            value={data.mobileNo}
-            required
-            className={styles.input}
+              type="text"
+              placeholder="Mobile No"
+              name="mobileNo"
+              onChange={handleChange}
+              value={data.mobileNo}
+              required
+              className={styles.input}
             />
             <input
-            type="text"
-            placeholder="Pin Code"
-            name="pinCode"
-            onChange={handleChange}
-            value={data.pinCode}
-            required
-            className={styles.input}
+              type="text"
+              placeholder="Pin Code"
+              name="pinCode"
+              onChange={handleChange}
+              value={data.pinCode}
+              required
+              className={styles.input}
             />
             <input
-            type="text"
-            placeholder="Address"
-            name="address"
-            onChange={handleChange}
-            value={data.address}
-            className={styles.input}
-            required
+              type="text"
+              placeholder="Address"
+              name="address"
+              onChange={handleChange}
+              value={data.address}
+              required
+              className={styles.input}
             />
             <input
-            type="text"
-            placeholder="Category"
-            name="category"
-            onChange={handleChange}
-            value={data.category}
-            className={styles.input}
-            required
+              type="text"
+              placeholder="Government-issued Registration number"
+              name="gov_id"
+              onChange={handleChange}
+              value={data.gov_id}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Category"
+              name="category"
+              onChange={handleChange}
+              value={data.category}
+              required
+              className={styles.input}
             />
             {error && <div className={styles.error_msg}>{error}</div>}
             <button type="submit" className={styles.green_btn}>
               Sign Up
             </button>
-            </form>
-            </div>
-            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 export default Signup;
